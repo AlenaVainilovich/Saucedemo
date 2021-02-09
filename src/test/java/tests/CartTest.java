e5ddf198564692c5fff64b4bcf01749affe58aac
tamika <vainilovich@gmail.com>
2021-02-09 17:34:47 +0300

    validateRemoveButtonInCart

--- src/test/java/tests/CartTest.java
+++ src/test/java/tests/CartTest.java
@@ -2,6 +2,7 @@ package tests;
 
 
 import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
 
 public class CartTest extends BaseTest {
@@ -13,6 +14,19 @@ public class CartTest extends BaseTest {
         productsPage.buyProduct("Sauce Labs Bike Light");
         cartPage.openPage();
         cartPage.productShouldBeInTheList("Sauce Labs Bike Light");
+
+    }
+
+    @Test
+    public void validateRemoveButtonInCart() {
+        loginPage.open();
+        loginPage.login("standard_user", "secret_sauce");
+        productsPage.buyProduct("Sauce Labs Bike Light");
+        productsPage.buyProduct("Sauce Labs Onesie");
+        productsPage.buyProduct("Sauce Labs Bolt T-Shirt");
+        cartPage.openPage();
+        cartPage.removeFromCart();
+        assertEquals(cartPage.getNumberOfItemsInCart(), 2);
 
     }
 }
